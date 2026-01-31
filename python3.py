f5c5c3f138e3578fe2ece3aa3ea0316369401f94
cholachat18-cmd <cholachat18@gmail.com>
2026-01-31 14:10:50 +0700

    Collection of Python

---
+++ python3.py
@@ -0,0 +1,55 @@
+"""
+#
+# Part: Collection of Python 
+# Lists, Tuples, Sets, Dictionaries
+#
+"""
+
+
+"""
+#
+# Lists
+#
+"""
+this_list = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(this_list)
+print(this_list[1]) #Accessing the second item
+print(type(this_list)) #Checking the type
+print(len(this_list)) #Length of the list
+
+"""
+#
+# Tuples
+#
+"""
+this_tuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(this_tuple)
+print(this_tuple[1]) #Accessing the second item
+print(type(this_tuple)) #Checking the type
+print(len(this_tuple)) #Length of the tuple
+
+"""
+#
+# Sets
+#
+"""
+this_set = {"apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"}
+print(this_set)
+print(type(this_set)) #Checking the type
+print(len(this_set)) #Length of the set
+
+"""
+#
+# Dictionaries
+#
+"""
+this_dict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+print(this_dict)
+print(this_dict["model"]) #Accessing the value of the "model"
+print(type(this_dict)) #Checking the type
+print(len(this_dict)) #Length of the dictionary
+
