93164eb926806589be52af08650bc34e3816f3fb
cholachat18-cmd <cholachat18@gmail.com>
2026-01-31 14:27:41 +0700

    Conditional Statements

---
+++ python4.py
@@ -0,0 +1,33 @@
+"""
+#
+# Conditional Statements
+#
+"""
+
+a = 10
+b = 20
+if a < b:
+    print("a is less than b")
+elif a == b:
+    print("a is equal to b")
+elif a <= b:
+    print("a is less than or equal to b")
+else:
+    print("a is greater than b")
+
+if a > b:
+    print("a is less than b")
+
+if a == b:
+    print("a is greater than b")
+
+if a < b:
+    print("a is less than b")
+elif a == b:
+    print("a is equal to b")
+
+if a < b:
+    print("a is less than b")
+else:             
+    print("a is greater than b")
+
