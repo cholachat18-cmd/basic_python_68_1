0d4b693e5e764f5cb6f6cb49520f2a87b9e52770
cholachat18-cmd <cholachat18@gmail.com>
2026-01-31 13:17:37 +0700

    Python Variables

---
+++ python1.py
@@ -0,0 +1,56 @@
+""""""
+#Part: Python Comment
+
+""""""
+#This is a single line comment in Python
+
+"""
+v = s / t
+ความเร็ว = ระยะทาง / เวลา
+v = ความเร็ว (m/s)
+s = ระยะทาง (m)
+t = เวลา (s) 
+"""
+print("Hello, World!")
+
+""""""
+#
+#Part: Python variables
+#
+""""""
+x = 10
+y = "10"
+print(x,y)
+
+x = str(5)
+y = int("5")
+z = float("5.0")
+print(type(x), type(y), type(z)) 
+
+""""""
+#
+#Part: variables Naming
+#
+""""""
+"""
+
+myvar = 100
+my_var = 200
+_my_var = 300                         #นิยมเป็นตัวเเปรเทมป์
+myVar = 400
+MYVAR = 500                           #นิยมตัวเเปร Global
+
+"""
+# Camel Case
+myVariableName = 900
+
+# Pascal Case
+MyVariableName = 1000
+
+# Snake
+
+
+
+
+
+
